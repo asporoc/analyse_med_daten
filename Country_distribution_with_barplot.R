2418e77c10bf0dbe4c25f4ed940dfba9745cc407
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2024-06-06 15:59:02 +0200

    Länder zahl und visualisierung

---
+++ Country_distribution_with_barplot.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(ggplot2)
+
+country_counts <- data %>%
+  count(Country)
+
+# Print the counts
+print(country_counts)
+
+ggplot(country_counts, aes(x = Country, y = n)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(title = "Count of Occurrences by Country",
+       x = "Country",
+       y = "Count") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
